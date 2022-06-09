c9674d7ed75a12d60bf004f23f309947078db0a6
Daniel Holmes <daniel.holmes@softwire.com>
2022-06-09 11:12:28 +0100

    Implement prestige levels

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -9,7 +9,8 @@ import { getWordOfLength, isValidWord } from "../../wordHelper";
 import { Button, Card } from "@mui/material";
 
 const winningLength = 8;
-const initialSecondsPerAnagram = 5;
+const initialBaseSecondsPerAnagram = 3;
+const additionalSecondsPerLetter = 0.5;
 const initialLivesLeft = 3;
 const initialWordLength = 2;
 
@@ -31,11 +32,15 @@ export type LetterOption = {
     pressed: number|undefined,
 };
 
+const getTimeForLevel = (baseSecondsPerAnagram: number, newWordLength: number): number =>
+    (baseSecondsPerAnagram + newWordLength * additionalSecondsPerLetter) * 1000;
+
 const Game: React.FC = () => {
     const [currentWord, setCurrentWord] = useState(getWordOfLength(initialWordLength));
     const [letterOptions, setLetterOptions] = useState<LetterOption[]>([]);
-    const [secondsPerAnagram, setSecondsPerAnagram] = useState(initialSecondsPerAnagram);
-    const [timeLeft, setTimeLeft] = useState(initialSecondsPerAnagram*1000);
+    const [baseSecondsPerAnagram, setBaseSecondsPerAnagram] = useState(initialBaseSecondsPerAnagram);
+    const [timeForLevel, setTimeForLevel] = useState(initialBaseSecondsPerAnagram*1000)
+    const [timeLeft, setTimeLeft] = useState(initialBaseSecondsPerAnagram*1000);
     const [gameStatus, setGameStatus] = useState(GameStatus.Ready);
     const [levelStatus, setLevelStatus] = useState(LevelStatus.InProgress);
     const [livesLeft, setLivesLeft] = useState(initialLivesLeft);
@@ -45,6 +50,7 @@ const Game: React.FC = () => {
     const getGuessWord = (letterOptions: LetterOption[]) => {
         return letterOptions.filter(lo => lo.pressed !== undefined).sort((a, b) => a.pressed! - b.pressed!).map(lo => lo.letter).join('');
     }
+    const prestigeLevel = initialBaseSecondsPerAnagram - baseSecondsPerAnagram + 1;
 
     const checkWord = (newLetterOptions: LetterOption[]): void => {
         const guessWord = getGuessWord(newLetterOptions);
@@ -75,27 +81,32 @@ const Game: React.FC = () => {
                 setLevelStatus(LevelStatus.InProgress);
             } else {
                 setTimeout(() => {
+                    const newWordLength = currentWord.length + 1;
+                    const timeForNewLevel = getTimeForLevel(baseSecondsPerAnagram, newWordLength);
                     setLevelStatus(LevelStatus.InProgress);
-                    setTimeLeft(secondsPerAnagram * 1000);
-                    const newWord = getWordOfLength(currentWord.length + 1);
-                    setCurrentWord(newWord);
+                    setTimeForLevel(timeForNewLevel);
+                    setTimeLeft(timeForNewLevel);
+                    setCurrentWord(getWordOfLength(newWordLength));
                 }, 500);
             }
         }
-    }, [currentWord, levelStatus, secondsPerAnagram]);
+    }, [currentWord, levelStatus, baseSecondsPerAnagram]);
 
     useEffect(() => {
         if (gameStatus === GameStatus.InProgress && levelStatus === LevelStatus.InProgress) {
+            const updateInterval = 100
             window.setTimeout(() => {
                 if (timeLeft > 0) {
-                    setTimeLeft(timeLeft - 10);
+                    setTimeLeft(timeLeft - updateInterval);
                 } else {
                     if (livesLeft > 0) {
                         setLevelStatus(LevelStatus.Lost);
                         setTimeout(() => {
-                            setTimeLeft(secondsPerAnagram * 1000);
-                            const newWord = getWordOfLength(Math.max(currentWord.length - 1, initialWordLength));
-                            setCurrentWord(newWord);
+                            const newWordLength = Math.max(currentWord.length - 1, initialWordLength);
+                            const timeForNewLevel = getTimeForLevel(baseSecondsPerAnagram, newWordLength);
+                            setTimeForLevel(timeForNewLevel);
+                            setTimeLeft(timeForNewLevel);
+                            setCurrentWord(getWordOfLength(newWordLength));
                             setLivesLeft(livesLeft - 1);
                             setLevelStatus(LevelStatus.InProgress);
                         }, 500);
@@ -103,10 +114,10 @@ const Game: React.FC = () => {
                         setGameStatus(GameStatus.Lost);
                     }
                 }
-            }, 10)
+            }, updateInterval)
         }
 
-    }, [timeLeft, levelStatus, gameStatus, livesLeft, currentWord, secondsPerAnagram]);
+    }, [timeLeft, levelStatus, gameStatus, livesLeft, currentWord, baseSecondsPerAnagram]);
 
     useEffect(() => {
         if (currentWord.length > highestWordLength) {
@@ -115,11 +126,12 @@ const Game: React.FC = () => {
     }, [currentWord, highestWordLength]);
 
     const resetGame = (newSecondsPerAnagram: number): void => {
-        setSecondsPerAnagram(newSecondsPerAnagram);
+        setBaseSecondsPerAnagram(newSecondsPerAnagram);
         setCurrentWord(getWordOfLength(initialWordLength));
         setLetterOptions([]);
         setGameStatus(GameStatus.InProgress);
-        setTimeLeft(newSecondsPerAnagram * 1000);
+        setTimeLeft(getTimeForLevel(newSecondsPerAnagram, initialWordLength));
+        setTimeForLevel(getTimeForLevel(newSecondsPerAnagram, initialWordLength));
         setLivesLeft(initialLivesLeft);
         setHighestWordLength(initialWordLength);
     };
@@ -149,14 +161,14 @@ const Game: React.FC = () => {
                 <Card className="EndGameMessage">
                     <p>Welcome to Danagrams.</p>
 
-                    <p>Find and type the anagram in 5 seconds.</p>
+                    <p>Find and type the anagram before the timer runs out.</p>
 
-                    <p>For each anagram you solve, another letter is added.</p>
+                    <p>For each anagram you solve, another letter is added and you get half a second longer.</p>
 
                     <p>Solve an 8 letter anagram to win!</p>
 
                     <Button
-                        onClick={() => resetGame(initialSecondsPerAnagram)}
+                        onClick={() => resetGame(initialBaseSecondsPerAnagram)}
                         size={"large"}
                     >
                         Play
@@ -167,34 +179,33 @@ const Game: React.FC = () => {
             return (<Card className="EndGameMessage">
                 <p>Nice work!</p>
 
-                <p>You completed the game with {secondsPerAnagram} seconds per anagram.</p>
+                <p>You completed the game on prestige level {prestigeLevel}.</p>
 
-                <p>Try it with {secondsPerAnagram - 1}?</p>
-
-                <Button
-                    onClick={() => resetGame(secondsPerAnagram - 1)}
-                    size={"large"}
-                >
-                    Go
-                </Button>
+                {(prestigeLevel === 3)
+                    ? <p>That's it! Go show off to your friends!</p>
+                    : <><p>The next prestige level has one less second per anagram.</p>
+                    <Button
+                        onClick={() => resetGame(baseSecondsPerAnagram - 1)}
+                        size={"large"}
+                    >
+                        Go
+                    </Button></>
+                }
             </Card>);
         case GameStatus.Lost:
-            const prestigeLevel = initialSecondsPerAnagram - secondsPerAnagram;
-            const prestigeMessage = prestigeLevel > 0
-                ? `You reached prestige level ${prestigeLevel} and had ${secondsPerAnagram} seconds per anagram!`
+            const prestigeMessage = prestigeLevel > 1
+                ? ` of prestige level ${prestigeLevel}`
                 : '';
             return (<div className="EndGameMessageContainer">
                 <Card className="EndGameMessage">
                     <p>Game over! The word was "{currentWord}".</p>
 
-                    <p>You reached level {highestWordLength}!</p>
-
-                    <p>{prestigeMessage}</p>
+                    <p>You reached level {highestWordLength}{prestigeMessage}!</p>
 
                     <p>Thanks for playing.</p>
 
                     <Button
-                        onClick={() => resetGame(initialSecondsPerAnagram)}
+                        onClick={() => resetGame(initialBaseSecondsPerAnagram)}
                         size={"large"}
                     >
                         Play again
@@ -205,7 +216,7 @@ const Game: React.FC = () => {
             return (<div className="Game">
                 <Timer
                     timeLeft={timeLeft}
-                    totalTime={secondsPerAnagram * 1000}
+                    totalTime={timeForLevel}
                     levelStatus={levelStatus}
                 />
                 <Output
